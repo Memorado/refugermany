6eab1cc1c80c6eef6792065afe9de422cb05d620
Tadas Ziemys <tadasz@gmail.com>
2015-09-29 14:01:34 +0200

    added initial webview to ArticlePage

---
+++ WelcomeGuide/WelcomeGuide/ArticlePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace WelcomeGuide
+{
+	public partial class ArticlePage : ContentPage
+	{
+		public ArticlePage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
